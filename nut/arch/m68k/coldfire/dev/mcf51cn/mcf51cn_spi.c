45560dcd7e9524c18b6a4ab0a3f947b29e77bedc
shindy <shindy@e18dba81-b7be-514b-945f-45d19d31607d>
2014-04-25 07:01:03 +0000

    Doplnění komentáře, proč se měnila rychlost SPI.
    Rychlost se měnila, protože byl nespolehlivý upgrade firmware. David si myslel, že to pomohlo a proto to commitnul. Nicméně poté zjistil, že to blbne když je vypnutá optimalizace a že to rychlostí SPI nejspíš není. Takže je ta úprava pravděpodobně zbytečná, ale nechci to měnit, aby se nemuselo vše zase testovat.
    
    git-svn-id: https://192.168.1.52/svn/svn/shared/nut_5.0.5/trunk/nut_5.0.5@5894 e18dba81-b7be-514b-945f-45d19d31607d

--- nut/arch/m68k/coldfire/dev/mcf51cn/mcf51cn_spi.c
+++ nut/arch/m68k/coldfire/dev/mcf51cn/mcf51cn_spi.c
@@ -239,7 +239,7 @@ void Mcf51cnSpiInit(void)
 	/* Init registers */
 	(void) MCF_SPI_S(SPI_CHANNEL); 		/* Read the status register */
 	(void) MCF_SPI_D(SPI_CHANNEL); 		/* Read the data register */
-	/* Set the baud rate register 0x41 for speed 1,25MHz */
+	/* Set the baud rate register, speed 1,25MHz */
 	/* Set the baud rate register 0x72 for speed 375kHz */
 	MCF_SPI_BR(SPI_CHANNEL) = 0x72; 	// TODO: pokud zadam nejvyzsi rychlost 12,5Mhz tak to zamrzne po dokonceni dlouheho zapisu nebo cteni
 	MCF_SPI_C2(SPI_CHANNEL) = 0x00; 	/* Configure the SPI port - control register 2 */
